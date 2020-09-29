c2a347c2645b77307b3d2243efd28088062b3525
Rickard Fahlquist (rf3252) <rickard.fahlquist@tobii.com>
2020-09-29 16:36:01 +0200

    Fix normalization and compiler flags

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -18,39 +18,53 @@ using namespace string_literals;
 
 const char header[] = R"(
 <html>
-<head> Dev and Rick's awesome thing </head>
-<body>
-<h1>Word Freqs</h1>
+     <head> Dev's and Rick's awesome thing </head>
+     <body>
+     <h1>Word Freqs</h1>
 )";
 
 const char tail[] = R"(
     </body>
-    </html>
+</html>
 )";
 
 class ParseFile
 {
 public:
+    struct Stats
+    {
+        int numWords = 0;
+
+    };
+
     ParseFile (string filename):filename_(filename)
     { cout << "ParseFile :" << filename_ << endl;
         fs_.open(filename_);
     }
 
+    // (1)
     void parse()
     {
 
         for (string line; getline(fs_, line);)
         {
             transform(line.begin(), line.end(), line.begin(), [](auto ch) -> auto {
-                if (isalpha(ch)) return ch; return ' ';
+                if (isalpha(ch)) {
+                    return ch;
+                }
+                else {
+                    return ' ';
+                }
             });
             auto buf  = istringstream{line};
             for (string word; buf >> word;) {
-                db_[word]++;
+                ++db_[word];
+                numWords_++;
             }
         }
     }
 
+    // (2)
     void sort_and_keep_n(int n)
     {
         vector<pair<string, unsigned>> freqs{db_.begin(), db_.end()};
@@ -62,13 +76,17 @@ public:
         copy_n(freqs.begin(), n, back_inserter(sorted_));
     }
 
+    // (3)
     void generate_html()
     {
-        auto largest = sorted_.front().second;
-        transform(sorted_.begin(), sorted_.end(), sorted_.begin(), [largest](auto& p) {
-            return make_pair(p.first, static_cast<unsigned>((p.second * 135. / largest) + 15.));
-        });
+        auto max = sorted_.front().second;
+        auto min = sorted_.back().second;
+        auto delta = max - min;
 
+        transform(sorted_.begin(), sorted_.end(), sorted_.begin(), [delta, min](auto& p) {
+            const auto normalized = (135. / delta) * (p.second - min) + 15.;
+            return make_pair(p.first, static_cast<unsigned>(normalized));
+        });
 
         transform(sorted_.begin(), sorted_.end(), back_inserter(spans_), [](auto& p){
             stringstream ss;
@@ -93,12 +111,17 @@ public:
         out << html.str();
     }
 
+    auto constexpr getNumWords() const {
+        return numWords_;
+    };
+
 private:
     string filename_;
     fstream fs_;
     unordered_map<string, unsigned> db_;
     vector<pair<string, unsigned>> sorted_;
     vector<string> spans_;
+    size_t numWords_ = 0;
 };
 
 
@@ -119,13 +142,13 @@ int main(int argc, char** argv)
     ParseFile pf(filename);
     const auto start = std::chrono::system_clock::now();
     pf.parse();
-    pf.sort_and_keep_n(10);
+    pf.sort_and_keep_n(50);
     pf.generate_html();
     const auto stop = std::chrono::system_clock::now();
     std::chrono::duration<double> diff = stop - start;
 
 
-    cout << "Duration: " << diff.count() << "s \n";
+    cout << "Processed " << pf.getNumWords() << " words in " << diff.count() << "seconds \n";
 
 
     return 0;
--- homework1/readme.txt
+++ homework1/readme.txt
@@ -1,3 +1,4 @@
 Use cmake command line to generate:
-$ cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=/usr/bin/g++-10 -G "CodeBlocks - Unix Makefiles" <path-to-CMakeLists.txt>
+$ mkdir build && cd build
+$ cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=/usr/bin/g++-10 -G "CodeBlocks - Unix Makefiles" ..
 
